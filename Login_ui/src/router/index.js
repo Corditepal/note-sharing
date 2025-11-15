15e62e43fe151a3044706c212d8aca5da000c79d
pkj04 <164497751+200410154831@users.noreply.github.com>
2025-11-15 19:45:22 +0800

    Add files via upload

---
+++ Login_ui/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AuthView from '../views/AuthView.vue'
+import UserDashboard from '../views/UserDashboard.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: AuthView,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: AuthView,
+  },
+  {
+    path: '/forgot-password',
+    name: 'ForgotPassword',
+    component: AuthView,
+  },
+  {
+    path: '/user',
+    name: 'UserDashboard',
+    component: UserDashboard,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
